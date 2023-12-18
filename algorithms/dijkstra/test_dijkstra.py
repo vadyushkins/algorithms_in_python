f624958843b09a8dc3311cf25d71060dabfea012
vadyushkins <vadim.i.abzalov@gmail.com>
2023-12-18 17:39:41 +0300

    Fix Dijkstra tests

--- algorithms/dijkstra/test_dijkstra.py
+++ algorithms/dijkstra/test_dijkstra.py
@@ -3,43 +3,43 @@ from dijkstra import dijkstra
 
 tests = [
     {
-        "graph": {
-            0: [[1, 1], [2, 1]],
-            1: [[0, 1], [2, 1]],
-            2: [[0, 1], [1, 1]],
+        "input": {
+            "graph": {
+                0: [[1, 1], [2, 1]],
+                1: [[0, 1], [2, 1]],
+                2: [[0, 1], [1, 1]],
+            },
+            "source_node": 0,
         },
-        "source_node": 0,
         "expected": [0, 1, 1],
     },
     {
-        "graph": {
-            0: [[1, 1]],
-            1: [[2, 1]],
-            2: [[3, 1]],
-            3: [],
+        "input": {
+            "graph": {
+                0: [[1, 1]],
+                1: [[2, 1]],
+                2: [[3, 1]],
+                3: [],
+            },
+            "source_node": 0,
         },
-        "source_node": 0,
         "expected": [0, 1, 2, 3],
     },
     {
-        "graph": {
-            0: [[1, 1], [2, 1]],
-            1: [[3, 1]],
-            2: [[3, 1]],
-            3: [],
+        "input": {
+            "graph": {
+                0: [[1, 1], [2, 1]],
+                1: [[3, 1]],
+                2: [[3, 1]],
+                3: [],
+            },
+            "source_node": 0,
         },
-        "source_node": 0,
         "expected": [0, 1, 1, 2],
     },
 ]
 
 
 @pytest.mark.parametrize("test", [i for i in range(len(tests))])
-def test_dijkstra(test):
-    assert (
-        dijkstra(
-            graph=tests[test]["graph"],
-            source_node=tests[test]["source_node"],
-        )
-        == tests[test]["expected"]
-    )
+def test(test):
+    assert dijkstra(**tests[test]["input"]) == tests[test]["expected"]
