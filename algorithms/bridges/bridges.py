b9e2687383efc86014554cd2b01d6bcf7afd712f
vadyushkins <vadim.i.abzalov@gmail.com>
2023-12-21 10:54:38 +0300

    Add Bridges Search algorithm

---
+++ algorithms/bridges/bridges.py
@@ -0,0 +1,42 @@
+from typing import Dict, List, Tuple
+
+
+def bridges(graph: Dict[int, List[int]]) -> List[Tuple[int, int]]:
+    n = len(graph)
+    visited = [False] * n
+    entry_time = [-1] * n
+    lowpoint = [-1] * n
+    timer = 0
+
+    bridges = []
+
+    def dfs(node: int, parent: int = -1):
+        nonlocal visited, entry_time, lowpoint, timer
+        visited[node] = True
+        entry_time[node] = timer
+        lowpoint[node] = timer
+        timer += 1
+
+        for neighbour in graph[node]:
+            if neighbour == parent:
+                continue
+
+            if visited[neighbour]:
+                lowpoint[node] = min(
+                    lowpoint[node],
+                    entry_time[neighbour],
+                )
+            else:
+                dfs(neighbour, parent=node)
+                lowpoint[node] = min(
+                    lowpoint[node],
+                    lowpoint[neighbour],
+                )
+                if lowpoint[neighbour] > entry_time[node]:
+                    bridges.append((node, neighbour))
+
+    for node in range(len(graph)):
+        if False == visited[node]:
+            dfs(node)
+
+    return bridges
---
+++ algorithms/bridges/test_bridges.py
@@ -0,0 +1,58 @@
+import pytest
+from bridges import bridges
+
+tests = [
+    {
+        "input": {
+            "graph": {
+                0: [1, 2],
+                1: [0],
+                2: [0],
+            },
+        },
+        "expected": [(0, 1), (0, 2)],
+    },
+    {
+        "input": {
+            "graph": {
+                0: [1, 2],
+                1: [0, 2],
+                2: [0, 1, 3],
+                3: [2],
+            },
+        },
+        "expected": [(2, 3)],
+    },
+    {
+        "input": {
+            "graph": {
+                0: [1, 5, 7],
+                1: [0, 2],
+                2: [1, 3, 5, 7],
+                3: [2, 4],
+                4: [3],
+                5: [0, 2, 6],
+                6: [5],
+                7: [0, 2, 8],
+                8: [7],
+            },
+        },
+        "expected": [(2, 3), (3, 4), (5, 6), (7, 8)],
+    },
+    {
+        "input": {
+            "graph": {
+                0: [1, 2],
+                1: [0, 2],
+                2: [0, 1],
+            },
+        },
+        "expected": [],
+    },
+]
+
+
+@pytest.mark.parametrize("test", [i for i in range(len(tests))])
+def test(test):
+    order = lambda pairs: sorted([(min(x, y), max(x, y)) for x, y in pairs])
+    assert order(bridges(**tests[test]["input"])) == order(tests[test]["expected"])
